4453ba1b7c3f69ad3de45631971681c001c0cca5
LBlend <24893890+LBlend@users.noreply.github.com>
2019-01-02 23:38:24 +0100

    some servers exceeds message character limit. ignores these servers

--- cogs/DevTools.py
+++ cogs/DevTools.py
@@ -31,19 +31,23 @@ class DevTools:
     @commands.command()
     async def scrapeservers(self, ctx):
         for guild in self.bot.guilds:
-            embed = discord.Embed(color=0xF02B30)
-            embed.add_field(name="Name", value=guild.name)
-            embed.add_field(name="ID", value=guild.id)
-            embed.add_field(name="Owner", value=f"<@{guild.owner.id}>")
-            embed.add_field(name="Region", value=guild.region)
-            embed.add_field(name="Creation date", value=guild.created_at)
-            embed.add_field(name=f"Channels ({len(guild.channels)})", value=[channel.name for channel in guild.channels], inline=False)
-            embed.add_field(name="ChannelIDs", value=[channel.id for channel in guild.channels], inline=False)
-            embed.add_field(name=f"Roles ({len(guild.roles)})", value=[role.name for role in guild.roles], inline=False)
-            embed.add_field(name=f"Members ({len(guild.members)})", value=[member.name for member in guild.members], inline=False)
-            embed.add_field(name=f"MemeberIDs", value=[member.id for member in guild.members], inline=False)
-            embed.set_thumbnail(url=guild.icon_url)
-            await ctx.send(embed=embed)
+            try:
+                embed = discord.Embed(color=0xF02B30)
+                embed.add_field(name="Name", value=guild.name)
+                embed.add_field(name="ID", value=guild.id)
+                embed.add_field(name="Owner", value=f"<@{guild.owner.id}>")
+                embed.add_field(name="Region", value=guild.region)
+                embed.add_field(name="Creation date", value=guild.created_at)
+                embed.add_field(name=f"Channels ({len(guild.channels)})", value=[channel.name for channel in guild.channels], inline=False)
+                embed.add_field(name="ChannelIDs", value=[channel.id for channel in guild.channels], inline=False)
+                embed.add_field(name=f"Roles ({len(guild.roles)})", value=[role.name for role in guild.roles], inline=False)
+                embed.add_field(name=f"Members ({len(guild.members)})", value=[member.name for member in guild.members], inline=False)
+                embed.add_field(name=f"MemeberIDs", value=[member.id for member in guild.members], inline=False)
+                embed.set_thumbnail(url=guild.icon_url)
+                await ctx.send(embed=embed)
+            except:
+                embed = discord.Embed(color=0xF02B30, description="ServerScraping feilet. Fortsetter om mulig...")
+                await ctx.send(embed=embed)
             
 
     @commands.is_owner()
