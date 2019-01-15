beca1ef241b41911ed22a0fcb09bdae7263837c9
LBlend <24893890+LBlend@users.noreply.github.com>
2019-01-15 22:25:58 +0100

    adds replies to messages without out the need for a prefix

---
+++ cogs/FunRepliesExample.py
@@ -0,0 +1,25 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+class FunReplies:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def react(self, message):
+        if message.author.bot:
+            return
+
+        if message.content.lower() == "morn":
+            await message.channel.send("Morn")
+
+        elif message.content.lower().startswith("no u"):
+            await message.channel.send(f"no u <@{message.author.id}>")
+
+        elif message.content.lower() == "nei du":
+            await message.channel.send(f"nei du <@{message.author.id}>")
+
+def setup(bot):
+    n = FunReplies(bot)
+    bot.add_listener(n.react, "on_message")
+    bot.add_cog(n)
