baa5f01ed63bb06c40d0b2814bb099096cb807cf
LBlend <24893890+LBlend@users.noreply.github.com>
2019-03-19 01:06:19 +0100

    dunno what to say really

--- cogs/Vær.py
+++ cogs/Vær.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 import codecs
 import json
 import requests
-import datetime
+from datetime import datetime
 import urllib.parse
 
 with codecs.open("config.json", "r", encoding="utf8") as f:
@@ -46,25 +46,27 @@ class Vær:
             link = f"https://openweathermap.org/city/{byId}"
             byName = data["name"]
             countryCode = data["sys"]["country"].lower()
-            weatherFetchDate = str(datetime.datetime.fromtimestamp(int(data["dt"])).strftime("%d.%m.%Y %H:%M"))
+            weatherFetchDate = datetime.fromtimestamp(data["dt"]).strftime("%d.%m.%Y %H:%M")
             description = data["weather"][0]["description"]
             tempCelcius = round((data["main"]["temp"]) - 273)
             tempFahrenheit = round(1.8 * ((data["main"]["temp"]) - 273) + 32)
             windSpeed = data["wind"]["speed"]
             humidity = data["main"]["humidity"]
             cloudiness = data["clouds"]["all"]
-            sunrise = str(datetime.datetime.fromtimestamp(int(data["sys"]["sunrise"])).strftime("%H:%M"))
-            sunset = str(datetime.datetime.fromtimestamp(int(data["sys"]["sunset"])).strftime("%H:%M"))
+            sunrise = datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%M")
+            sunset = datetime.fromtimestamp(data["sys"]["sunset"]).strftime("%H:%M")
+            nowTime = datetime.now().strftime("%d.%m.%Y %H:%M")
 
             #   Embed
             embed = discord.Embed(title=f":flag_{countryCode}: {byName}, {countryCode.upper()} | {weatherFetchDate} (Norsk Tid)", color=0x0085ff, url=link, description=description)
+            embed.set_author(name="OpenWeatherMap", icon_url="https://pbs.twimg.com/profile_images/720298646630084608/wb7LSoAc_400x400.jpg")
             embed.add_field(name="Temperatur", value=f"{tempCelcius} °C\n{tempFahrenheit} °F")
             embed.add_field(name="Vind", value=f"{windSpeed} m/s")
             embed.add_field(name="Luftfuktighet", value=f"{humidity}%")
             embed.add_field(name="Skyer", value=f"{cloudiness}%")
             embed.add_field(name="Soloppgang (Norsk tid)", value=sunrise)
             embed.add_field(name="Solnedgang (Norsk tid)", value=sunset)
-            embed.set_author(name=f"{ctx.message.author.name}#{ctx.message.author.discriminator}", icon_url=ctx.message.author.avatar_url)
+            embed.set_footer(text=f"Tid i Norge nå: {nowTime}")
             await statusmsg.edit(embed=embed)
 
 
