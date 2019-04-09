55552c625be9c39bc478b5fed7045bf2a62dc805
LBlend <24893890+LBlend@users.noreply.github.com>
2019-04-09 19:18:33 +0200

    rewrite migration

--- cogs/NSFW.py
+++ cogs/NSFW.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 import requests
 
-class NSFW:
+class NSFW(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -17,17 +17,14 @@ class NSFW:
         embed = discord.Embed(description="Laster...")
         statusmsg = await ctx.send(embed=embed)
 
-        #   Sjekk NSFW
         if not ctx.channel.is_nsfw():
             embed = discord.Embed(color=0xFF0000, description=":x: Du må være i en NSFW-kanal")
             await statusmsg.edit(embed=embed)
             return
 
-        #   Hent data
         data = requests.get("https://nekos.life/api/v2/img/hololewd").json()
         hololewd = data["url"]
 
-        #   Embed
         embed = discord.Embed(color=0x0085ff)
         embed.set_image(url=hololewd)
         await statusmsg.edit(embed=embed)
@@ -41,17 +38,14 @@ class NSFW:
         embed = discord.Embed(description="Laster...")
         statusmsg = await ctx.send(embed=embed)
 
-        #   Sjekk NSFW
         if not ctx.channel.is_nsfw():
             embed = discord.Embed(color=0xFF0000, description=":x: Du må være i en NSFW-kanal")
             await statusmsg.edit(embed=embed)
             return
 
-        #   Hent data
         data = requests.get("https://nekos.life/api/v2/img/holoero").json()
         holoero = data["url"]
 
-        #   Embed
         embed = discord.Embed(color=0x0085ff)
         embed.set_image(url=holoero)
         await statusmsg.edit(embed=embed)
@@ -64,17 +58,14 @@ class NSFW:
         embed = discord.Embed(description="Laster...")
         statusmsg = await ctx.send(embed=embed)
 
-        #   Sjekk NSFW
         if not ctx.channel.is_nsfw():
             embed = discord.Embed(color=0xFF0000, description=":x: Du må være i en NSFW-kanal")
             await statusmsg.edit(embed=embed)
             return
 
-        #   Hent data
         data = requests.get("https://nekos.life/api/v2/img/bj").json()
         bj = data["url"]
 
-        #   Embed
         embed = discord.Embed(color=0x0085ff)
         embed.set_image(url=bj)
         await statusmsg.edit(embed=embed)
@@ -87,17 +78,14 @@ class NSFW:
         embed = discord.Embed(description="Laster...")
         statusmsg = await ctx.send(embed=embed)
 
-        #   Sjekk NSFW
         if not ctx.channel.is_nsfw():
             embed = discord.Embed(color=0xFF0000, description=":x: Du må være i en NSFW-kanal")
             await statusmsg.edit(embed=embed)
             return
 
-        #   Hent data
         data = requests.get("https://nekos.life/api/v2/img/yuri").json()
         yuri = data["url"]
 
-        #   Embed
         embed = discord.Embed(color=0x0085ff)
         embed.set_image(url=yuri)
         await statusmsg.edit(embed=embed)
@@ -110,21 +98,18 @@ class NSFW:
         embed = discord.Embed(description="Laster...")
         statusmsg = await ctx.send(embed=embed)
 
-        #   Sjekk NSFW
         if not ctx.channel.is_nsfw():
             embed = discord.Embed(color=0xFF0000, description=":x: Du må være i en NSFW-kanal")
             await statusmsg.edit(embed=embed)
             return
 
-        #   Hent data
         data = requests.get("https://nekos.life/api/v2/img/trap").json()
         trap = data["url"]
 
-        #   Embed
         embed = discord.Embed(color=0x0085ff)
         embed.set_image(url=trap)
         await statusmsg.edit(embed=embed)
-    
+
 
 def setup(bot):
     bot.add_cog(NSFW(bot))
