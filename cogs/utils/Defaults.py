ed60e7940c265ea1a7689c262532b4755b2bf37c
LBlend <24893890+LBlend@users.noreply.github.com>
2019-07-31 20:08:35 +0200

    pythonic if statements

--- cogs/utils/Defaults.py
+++ cogs/utils/Defaults.py
@@ -5,7 +5,7 @@ async def error_fatal_edit(ctx, status_msg, text, *, mention=True):
     embed = discord.Embed(color=0xFF0000, description=f'❌ {text}')
     embed.set_footer(icon_url=ctx.author.avatar_url, text=f'{ctx.author.name}#{ctx.author.discriminator}')
 
-    if mention is True:
+    if mention:
         return await status_msg.edit(content=ctx.author.mention, embed=embed)
 
     return await status_msg.edit(embed=embed)
@@ -15,7 +15,7 @@ async def error_warning_edit(ctx, status_msg, text, *, mention=True):
     embed = discord.Embed(color=0xF1C40F, description=f'⚠️ {text}')
     embed.set_footer(icon_url=ctx.author.avatar_url, text=f'{ctx.author.name}#{ctx.author.discriminator}')
 
-    if mention is True:
+    if mention:
         return await status_msg.edit(content=ctx.author.mention, embed=embed)
 
     return await status_msg.edit(embed=embed)
@@ -25,7 +25,7 @@ async def error_fatal_send(ctx, text, *, mention=False):
     embed = discord.Embed(color=0xFF0000, description=f'❌ {text}')
     embed.set_footer(icon_url=ctx.author.avatar_url, text=f'{ctx.author.name}#{ctx.author.discriminator}')
 
-    if mention is True:
+    if mention:
         return await ctx.send(content=ctx.author.mention, embed=embed)
 
     return await ctx.send(embed=embed)
@@ -35,7 +35,7 @@ async def error_warning_send(ctx, text, *, mention=False):
     embed = discord.Embed(color=0xF1C40F, description=f'⚠️ {text}')
     embed.set_footer(icon_url=ctx.author.avatar_url, text=f'{ctx.author.name}#{ctx.author.discriminator}')
 
-    if mention is True:
+    if mention:
         return await ctx.send(content=ctx.author.mention, embed=embed)
 
     return await ctx.send(embed=embed)
