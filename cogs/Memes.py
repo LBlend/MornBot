b6c9fd7b8cf20e32326c4e1c4bb7fc3223f578c0
LBlend <24893890+LBlend@users.noreply.github.com>
2019-08-11 13:51:02 +0200

    egon mem

--- cogs/Memes.py
+++ cogs/Memes.py
@@ -69,6 +69,49 @@ class Memes(commands.Cog):
             except:
                 pass
 
+    @commands.bot_has_permissions(embed_links=True, attach_files=True)
+    @commands.cooldown(1, 5, commands.BucketType.guild)
+    @commands.command()
+    async def egon(self, ctx, dorli_tekst: str, bra_tekst: str):
+        """Egon mem"""
+
+        async with ctx.channel.typing():
+            
+            font = './assets/fonts/Roboto_Mono/RobotoMono-Medium.ttf'
+            top_text, top_font = format_text(dorli_tekst, 800, font)
+            bottom_text, bottom_font = format_text(bra_tekst, 800, font)
+
+            image = Image.open(f'./assets/egon.png').convert('RGBA')
+            color = (0, 0, 0, 255)
+
+            top = Image.new('RGBA', (493, 423), (255, 255, 255, 255))
+            draw = ImageDraw.Draw(top)
+            font_size = draw.multiline_textsize(top_text, font=top_font)
+            font_size = ((top.size[0] - font_size[0]) / 2, (top.size[1] - font_size[1]) / 2)
+            draw.multiline_text(font_size, top_text, font=top_font, fill=color, align='center')
+            image.paste(top, (531, 0))
+
+            bottom = Image.new('RGBA', (493, 487), (255, 255, 255, 255))
+            draw = ImageDraw.Draw(bottom)
+            font_size = draw.multiline_textsize(bottom_text, font=bottom_font)
+            font_size = ((bottom.size[0] - font_size[0]) / 2, (bottom.size[1] - font_size[1]) / 2)
+            draw.multiline_text(font_size, bottom_text, font=bottom_font, fill=color, align='center')
+            image.paste(bottom, (531, 432))
+
+            image.save(f'./assets/{ctx.author.id}_edit.png')
+
+            f = discord.File(f'./assets/{ctx.author.id}_edit.png')
+            embed = discord.Embed()
+            embed.set_image(url=f'attachment://{ctx.author.id}_edit.png')
+            await Defaults.set_footer(ctx, embed)
+            await ctx.send(embed=embed, file=f)
+
+            try:
+                remove(f'./assets/{ctx.author.id}_raw.png')
+                remove(f'./assets/{ctx.author.id}_edit.png')
+            except:
+                pass
+
 
 def setup(bot):
     bot.add_cog(Memes(bot))
