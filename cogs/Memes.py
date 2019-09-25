085622197b7fe2da32fccbc1124e7ba788e3c450
LBlend <24893890+LBlend@users.noreply.github.com>
2019-09-25 18:05:06 +0200

    added harald meme

--- cogs/Memes.py
+++ cogs/Memes.py
@@ -108,6 +108,49 @@ class Memes(commands.Cog):
             except:
                 pass
 
+    @commands.bot_has_permissions(embed_links=True, attach_files=True)
+    @commands.cooldown(1, 5, commands.BucketType.guild)
+    @commands.command(aliases=['konge'])
+    async def harald(self, ctx, dorli_tekst: str, bra_tekst: str):
+        """Desanti mem"""
+
+        async with ctx.channel.typing():
+            
+            font = './assets/fonts/Roboto_Mono/RobotoMono-Medium.ttf'
+            top_text, top_font = format_text(dorli_tekst, 500, font)
+            bottom_text, bottom_font = format_text(bra_tekst, 500, font)
+
+            image = Image.open(f'./assets/meme_templates/harald.png').convert('RGBA')
+            color = (0, 0, 0, 255)
+
+            top = Image.new('RGBA', (301, 382), (255, 255, 255, 255))
+            draw = ImageDraw.Draw(top)
+            font_size = draw.multiline_textsize(top_text, font=top_font)
+            font_size = ((top.size[0] - font_size[0]) / 2, (top.size[1] - font_size[1]) / 2)
+            draw.multiline_text(font_size, top_text, font=top_font, fill=color, align='center')
+            image.paste(top, (383, 0))
+
+            bottom = Image.new('RGBA', (301, 383), (255, 255, 255, 255))
+            draw = ImageDraw.Draw(bottom)
+            font_size = draw.multiline_textsize(bottom_text, font=bottom_font)
+            font_size = ((bottom.size[0] - font_size[0]) / 2, (bottom.size[1] - font_size[1]) / 2)
+            draw.multiline_text(font_size, bottom_text, font=bottom_font, fill=color, align='center')
+            image.paste(bottom, (383, 385))
+
+            image.save(f'./assets/temp/{ctx.author.id}_edit.png')
+
+            f = discord.File(f'./assets/temp/{ctx.author.id}_edit.png')
+            embed = discord.Embed()
+            embed.set_image(url=f'attachment://{ctx.author.id}_edit.png')
+            await Defaults.set_footer(ctx, embed)
+            await ctx.send(embed=embed, file=f)
+
+            try:
+                remove(f'./assets/temp/{ctx.author.id}_raw.png')
+                remove(f'./assets/temp/{ctx.author.id}_edit.png')
+            except:
+                pass
+
 
 def setup(bot):
     bot.add_cog(Memes(bot))
