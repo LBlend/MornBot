21102432eeda8f2dc26a605c1497694153acc48b
LBlend <24893890+LBlend@users.noreply.github.com>
2020-04-29 14:46:46 +0200

    4wc

--- cogs/TempCog.py
+++ cogs/TempCog.py
@@ -18,6 +18,15 @@ class TempCog(commands.Cog):
         await message.add_reaction('👍')
         await message.add_reaction('👎')
 
+    @commands.command(name='4wc')
+    async def _4wc(self, ctx):
+
+        if ctx.channel.id == 569615483184349193:
+            role_4wc = ctx.guild.get_role(704962348502482984)
+            await ctx.author.add_roles(role_4wc)
+            await ctx.send('👍', delete_after=5.0)
+            await ctx.message.delete()
+
 
 def setup(bot):
     bot.add_listener(TempCog(bot).on_message, 'on_message')
